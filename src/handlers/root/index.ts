449cf675682a393ff04f75197d7c13ea1082fe34
Christopher Little <clittle@gsma.com>
2020-09-08 14:54:10 +0100

    Remove enrolment and verification routes
    Within our call center application, having dedicated routes for
    enrolment and verification was actually a little limiting. In
    particular, it makes it difficult to have multiple entry-points
    (necessary for alert callbacks) without duplicating the verification
    routes. Here I removed the dedicated routes, to replace them later with
    a different solution.

--- src/handlers/root/index.ts
+++ src/handlers/root/index.ts
@@ -3,19 +3,11 @@ import { getVoiceParams, __ } from '../../services/strings';
 import { safeHandle } from '../../services/errors';
 
 export const get = safeHandle(async (request) => {
-    const { language, user } = request;
+    const { language } = request;
 
     const response = new twiml.VoiceResponse();
-    if (user.isEnrolled) {
-        response.say(getVoiceParams(language), __('welcome-known', language));
-        response.redirect({ method: 'GET' }, `./${language}/verify`);
-    } else {
-        response.say(
-            getVoiceParams(language),
-            __('welcome-stranger', language)
-        );
-        response.redirect({ method: 'GET' }, `./${language}/enrol`);
-    }
+    response.say(getVoiceParams(language), __('welcome-known', language));
+    response.redirect({ method: 'GET' }, `./${language}/menu`);
 
     return response;
 });
