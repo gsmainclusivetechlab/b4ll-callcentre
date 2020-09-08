08fb91d5ecd72ba5717f8d2875607e75e03cc32c
Christopher Little <clittle@gsma.com>
2020-09-08 15:09:14 +0100

    Use new auth on menu handler and update strings
    Now that we have added the `requireVerification` parameter, it's time to
    use it. Here I simply add it to the menu handler to prevent access to
    the account menu before the user's voice has been verified. I also
    updated some strings to make the flow slightly more natural, and added
    missing languages.

--- src/handlers/menu/index.ts
+++ src/handlers/menu/index.ts
@@ -28,19 +28,31 @@ const menu: MenuOption[] = [
     },
 ];
 
-export const get = safeHandle(async (request) => {
-    const { language } = request;
+export const get = safeHandle(
+    async (request) => {
+        const { language } = request;
 
-    const response = new twiml.VoiceResponse();
+        const response = new twiml.VoiceResponse();
 
-    menuToGather(response, request, menu);
+        menuToGather(response, request, menu);
 
-    // if the gather doesn't detect anything, we fall back on this next instruction:
-    response.say(getVoiceParams(language), __('did-not-understand', language));
-    response.redirect({ method: 'GET' }, `./menu`);
-    return response;
-});
+        // if the gather doesn't detect anything, we fall back on this next instruction:
+        response.say(
+            getVoiceParams(language),
+            __('did-not-understand', language)
+        );
+        response.redirect({ method: 'GET' }, `./menu`);
+        return response;
+    },
+    { requireVerification: true }
+);
 
-export const post = safeHandle(async (request) => {
-    return menuToHandler(menu, request, `./menu`);
-});
+export const post = safeHandle(
+    async (request) => {
+        return menuToHandler(menu, request, `./menu`);
+    },
+    {
+        requireVerification: true,
+        loginRedirect: { method: 'GET', target: './menu' },
+    }
+);
