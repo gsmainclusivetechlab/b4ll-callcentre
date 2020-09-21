90fe9661ed3f6b277575f335ef771b51fb2ebe22
Samy <sbouchlaghem@gsma.com>
2020-09-21 11:38:34 +0100

    Use VoiceIt wrapper library

--- src/engine/voiceit/api.ts
+++ src/engine/voiceit/api.ts
@@ -1,145 +1,154 @@
+import VoiceIt2 from 'voiceit2-nodejs';
 import { VoiceIt } from 'voiceit2-nodejs';
-import axios from 'axios';
 
 export const REQUIRED_ENROLMENTS = 3;
 
-const auth = {
-    username: process.env.VOICEIT_API_KEY || '',
-    password: process.env.VOICEIT_API_TOKEN || '',
-};
-const api = axios.create({
-    baseURL: 'https://api.voiceit.io/',
-    auth,
-});
-
-const myVoiceIt = new VoiceIt(
-    process.env.VOICEIT_API_KEY,
-    process.env.VOICEIT_API_TOKEN
+const myVoiceIt = new VoiceIt2(
+    process.env.VOICEIT_API_KEY || '',
+    process.env.VOICEIT_API_TOKEN || ''
 );
 
 export async function createUser(): Promise<VoiceIt.Response<VoiceIt.User>> {
-    const result = await api.post('/users');
-    return result.data;
-}
-
-export async function createUser_new(): Promise<
-    VoiceIt.Response<VoiceIt.User>
-> {
-    return myVoiceIt.createUser((jsonResponse) => {
-        if (
-            jsonResponse['status'] === 200 &&
-            jsonResponse['responseCode'] === 'SUCC'
-        ) {
-            return jsonResponse;
-        } else {
-            return 'Failed to create new user'; //todo: change this
-        }
+    return new Promise<VoiceIt.Response<VoiceIt.User>>((resolve) => {
+        myVoiceIt.createUser((callback) => {
+            resolve(callback);
+        });
     });
 }
-
-export async function getPhrases(
-    language: string
-): Promise<VoiceIt.VoiceEnrolment[]> {
-    const result = await api.get(`/phrases/${language}`);
-    return result.data.phrases || [];
-}
-
 interface ArrayEnrolment {
     createdAt: string;
     contentLanguage: string;
     voiceEnrollmentId: number;
     text: string;
 }
-export async function getPhrases_new(
-    language: string
-): Promise<VoiceIt.Phrase[]> {
-    return myVoiceIt.getPhrases(
-        { contentLanguage: language },
-        (jsonResponse) => {
-            if (
-                jsonResponse['status'] === 200 &&
-                jsonResponse['responseCode'] === 'SUCC'
-            ) {
-                return jsonResponse['phrases'];
-            } else {
-                return 'Failed to get phrases'; //todo: change this
+
+export async function getPhrases(
+    language: VoiceIt.ContentLanguage
+): Promise<
+    VoiceIt.Response<{
+        count: number;
+        phrases: VoiceIt.Phrase[];
+    }>
+> {
+    return new Promise<
+        VoiceIt.Response<{
+            count: number;
+            phrases: VoiceIt.Phrase[];
+        }>
+    >((resolve) => {
+        myVoiceIt.getPhrases(
+            {
+                contentLanguage: language,
+            },
+            (callback) => {
+                resolve(callback);
             }
-        }
-    );
+        );
+    });
 }
 
 export async function getEnrolledPhrases(
     userId: string,
     language: string
 ): Promise<Record<string, number>> {
-    const results = await myVoiceIt.getAllVoiceEnrollments(
-        {
-            userId: userId,
-        },
-        (jsonResponse) => {
-            if (
-                jsonResponse['status'] === 200 &&
-                jsonResponse['responseCode'] === 'SUCC'
-            ) {
-                return jsonResponse;
+    let results = {
+        message:
+            'Successfully got all voice enrollments for user with userId : usr_feb6d1fcd80448628db8ec6a7ddb6322',
+        count: 2,
+        status: 200,
+        timeTaken: '0.079s',
+        voiceEnrollments: [
+            {
+                createdAt: 1487026658003,
+                contentLanguage: 'en-US',
+                voiceEnrollmentId: 73,
+                text: 'never forget tomorrow is a new day',
+            },
+        ],
+        responseCode: 'SUCC',
+    };
+
+    return new Promise<Record<string, number>>((resolve) => {
+        myVoiceIt.getAllVoiceEnrollments(
+            {
+                userId: userId,
+            },
+            (callback) => {
+                results = callback;
+                const phrases = ((results.voiceEnrollments as unknown) as ArrayEnrolment[])
+                    .map(
+                        ([
+                            id,
+                            encodedPhrase,
+                            contentLanguage,
+                            createdAt,
+                        ]): VoiceIt.VoiceEnrolment => ({
+                            voiceEnrollmentId: id,
+                            contentLanguage,
+                            createdAt,
+                            text: Buffer.from(encodedPhrase, 'base64').toString(
+                                'utf8'
+                            ),
+                        })
+                    )
+                    .filter((x) => x.contentLanguage === language)
+                    .map((x) => x.text);
+                resolve(
+                    phrases.reduce(
+                        (acc, curr) => ({
+                            ...acc,
+                            [curr]: (acc[curr] || 0) + 1,
+                        }),
+                        {} as Record<string, number>
+                    )
+                );
             }
-        }
-    );
-    const phrases = (results['voiceEnrollments'] as ArrayEnrolment[])
-        .map(
-            ([
-                id,
-                encodedPhrase,
-                contentLanguage,
-                createdAt,
-            ]): VoiceIt.VoiceEnrolment => ({
-                voiceEnrolmentId: id,
-                contentLanguage,
-                createdAt,
-                text: Buffer.from(encodedPhrase, 'base64').toString('utf8'),
-            })
-        )
-        .filter((x) => x.contentLanguage === language)
-        .map((x) => x.text);
-    return phrases.reduce(
-        (acc, curr) => ({
-            ...acc,
-            [curr]: (acc[curr] || 0) + 1,
-        }),
-        {} as Record<string, number>
-    );
+        );
+    });
 }
 
 export async function enrolUser(
     userId: string,
-    language: string,
+    language: VoiceIt.ContentLanguage,
     enrolment: {
         phrase: string;
         recordingUrl: string;
     }
 ): Promise<VoiceIt.Response<VoiceIt.VoiceEnrolment>> {
-    const result = await api.post('/enrollments/voice/byUrl', {
-        userId,
-        phrase: enrolment.phrase,
-        fileUrl: enrolment.recordingUrl,
-        contentLanguage: language,
+    return new Promise<VoiceIt.Response<VoiceIt.VoiceEnrolment>>((resolve) => {
+        myVoiceIt.createVoiceEnrollmentByUrl(
+            {
+                userId: userId,
+                contentLanguage: language,
+                phrase: enrolment.phrase,
+                audioFileURL: enrolment.recordingUrl,
+            },
+            (jsonResponse) => {
+                resolve(jsonResponse);
+            }
+        );
     });
-    return result.data;
 }
 
 export async function verifyUser(
     userId: string,
-    language: string,
+    language: VoiceIt.ContentLanguage,
     enrolment: {
         phrase: string;
         recordingUrl: string;
     }
 ): Promise<VoiceIt.Response<{ confidence: number }>> {
-    const result = await api.post('/verification/voice/byUrl', {
-        userId,
-        phrase: enrolment.phrase,
-        fileUrl: enrolment.recordingUrl,
-        contentLanguage: language,
+    return new Promise<VoiceIt.Response<{ confidence: number }>>((resolve) => {
+        myVoiceIt.voiceVerificationByUrl(
+            {
+                userId,
+                phrase: enrolment.phrase,
+                audioFileURL: enrolment.recordingUrl,
+                contentLanguage: language,
+            },
+            (callback) => {
+                resolve(callback);
+            }
+        );
     });
-    return result.data;
 }
