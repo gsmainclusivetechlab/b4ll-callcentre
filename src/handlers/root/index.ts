73846a2f44cf9087040d00c4b6881bc053482ab2
Christopher Little <clittle@gsma.com>
2020-11-19 14:33:59 +0000

    Add hardcoded number restrictions

--- src/handlers/root/index.ts
+++ src/handlers/root/index.ts
@@ -1,11 +1,22 @@
 import { twiml } from 'twilio';
 import { getVoiceParams, __ } from '../../services/strings';
 import { safeHandle } from '../../services/safeHandle';
+import approvedCallers from '../../numbers.json';
 
 export const get = safeHandle(async (request) => {
     const { language, user } = request;
 
     const response = new twiml.VoiceResponse();
+
+    if (approvedCallers.indexOf(user.id) < 0) {
+        response.say(
+            getVoiceParams(language),
+            __('unapproved-caller', language)
+        );
+        response.hangup();
+        return response;
+    }
+
     response.say(getVoiceParams(language), __('welcome-known', language));
 
     if (user.isDeactivated) {
