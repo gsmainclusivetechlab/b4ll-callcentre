a1a93aac0a187f312d658c05949e5f19660da916
Samy <sbouchlaghem@gsma.com>
2022-02-03 17:15:54 +0000

    uc03 cash in/cash out

---
+++ src/handlers/agent/cash-in/index.ts
@@ -0,0 +1,73 @@
+import { twiml } from 'twilio';
+import { getVoiceParams, __ } from '../../../services/strings';
+import { safeHandle } from '../../../services/safeHandle';
+
+export const get = safeHandle(
+    async (request) => {
+        const response = new twiml.VoiceResponse();
+        const { user, language } = request;
+        const cashInAmount = user.transferValue;
+
+        if (typeof cashInAmount === 'number') {
+            if (user.balanceAmount) {
+                if (user.balanceAmount >= cashInAmount) {
+                    const gather = response.gather({
+                        input: ['dtmf'],
+                        numDigits: 1,
+                        action: 'cash-in/confirmation',
+                    });
+                    gather.say(
+                        getVoiceParams(language),
+                        __(
+                            'cash-in-confirmation',
+                            {
+                                value: cashInAmount,
+                                account: user.transferAccount
+                                    ?.split('')
+                                    .join(' '),
+                            },
+                            language
+                        )
+                    );
+
+                    // If the user doesn't enter input, loop
+                    response.say(
+                        getVoiceParams(language),
+                        __('did-not-understand', language)
+                    );
+                    response.redirect({ method: 'GET' }, '../cash-out');
+                    return response;
+                } else {
+                    response.say(
+                        getVoiceParams(language),
+                        __('cash-in-value-error', language)
+                    );
+                    response.redirect({ method: 'GET' }, `../../../return`);
+                }
+            } else {
+                response.say(
+                    getVoiceParams(language),
+                    __(
+                        'error',
+                        {
+                            message:
+                                'Your account balance is 0. Please check your balance to reset it.',
+                        },
+                        language
+                    )
+                );
+                response.redirect({ method: 'GET' }, `../../../return`);
+            }
+        } else {
+            response.say(
+                getVoiceParams(language),
+                __('cash-in-value-error', language)
+            );
+            response.hangup();
+        }
+        return response;
+    },
+    {
+        requireVerification: true,
+    }
+);
---
+++ src/handlers/sms/agent/cash-out/index.ts
@@ -0,0 +1,99 @@
+import twilio, { twiml } from 'twilio';
+import {
+    SupportedLanguage,
+    getVoiceParams,
+    __,
+} from '../../../../services/strings';
+import { safeHandle } from '../../../../services/safeHandle';
+import { VerificationState } from '../../../../services/auth';
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const apiHost = process.env.API_HOST;
+
+const twilioClient = twilio(accountSid, authToken);
+const prefixByLanguage = (language: SupportedLanguage) => {
+    switch (language) {
+        case 'en-DEV':
+        case 'en-GB':
+            return '+44';
+        case 'fr-FR':
+            return '+33';
+    }
+};
+
+export const get = safeHandle(async ({ language, user, auth, event }) => {
+    if (auth.state !== VerificationState.REGISTERED) {
+        throw {
+            statusCode: 500,
+            error: __('alert-unregistered', language),
+        };
+    }
+    const message = new twiml.VoiceResponse();
+    const ivrNumber = event.queryStringParameters?.To;
+    const cashInAmount = user.transferValue;
+
+    message.say(
+        getVoiceParams(language),
+        __('sms-agent-cash-out-welcome', { amount: cashInAmount }, language)
+    );
+    message.redirect(
+        { method: 'POST' },
+        `${apiHost}${language}/sms/agent/cash-out`
+    );
+
+    if (!ivrNumber) {
+        const numbers = await twilioClient.incomingPhoneNumbers.list({
+            phoneNumber: prefixByLanguage(language),
+            limit: 1,
+        });
+        if (!numbers[0])
+            throw new Error(
+                'Could not find a corresponding number to call from'
+            );
+
+        await twilioClient.calls.create({
+            twiml: message.toString(),
+            to: user.id,
+            from: numbers[0].phoneNumber,
+        });
+    } else {
+        // TODO: pick a phone number in a region that matches the user's
+        const numbers = await twilioClient.incomingPhoneNumbers.list({
+            phoneNumber: prefixByLanguage(language),
+        });
+
+        const twilioNumber = numbers.find((i) => i.phoneNumber === ivrNumber);
+
+        console.log(ivrNumber, twilioNumber);
+
+        if (!twilioNumber)
+            throw new Error(
+                'Could not find a corresponding number to call from'
+            );
+
+        await twilioClient.calls.create({
+            twiml: message.toString(),
+            to: user.id,
+            from: twilioNumber.phoneNumber,
+        });
+    }
+
+    return message;
+});
+
+export const post = safeHandle(
+    async ({ language }) => {
+        const message = new twiml.VoiceResponse();
+        message.redirect(
+            { method: 'GET' },
+            `${apiHost}${language}/agent/cash-out`
+        );
+        return message;
+    },
+    {
+        allowEnrolment: false,
+        requireVerification: false,
+        allowDeactivated: false,
+    }
+);
